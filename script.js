dd2d233277f5daf44a5377eb73bd5eb9af1d54d3
techcomkathia <techcomk@gmail.com>
2024-07-12 11:27:10 -0300

    feat: initial commit :tada:

---
+++ script.js
@@ -0,0 +1,37 @@
+// Função assincrona ou síncrona, o que é?
+
+// Função assíncrona é uma função que não é executada imediatamente, ela é executada em um momento futuro, ou seja, ela é executada em um momento posterior.
+
+// Função síncrona é uma função que é executada imediatamente, ou seja, ela é executada no momento em que é chamada.
+
+// Exemplo de função assíncrona
+
+// async function minhaFuncao() {
+//     return "Irei pagar a pizza";
+// }
+
+// console.log(minhaFuncao());
+
+// Exemplo de função síncrona
+
+// function minhaFuncao() {
+//     return "Irei pagar a pizza";
+// }
+
+// console.log(minhaFuncao());
+
+// Exemplo de função assíncrona com await e setTimeout
+
+async function minhaFuncao() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Irei pagar a pizza");
+        }, 3000);
+    });
+}
+
+async function executaFuncao() {
+    console.log(await minhaFuncao());
+}
+
+executaFuncao();
